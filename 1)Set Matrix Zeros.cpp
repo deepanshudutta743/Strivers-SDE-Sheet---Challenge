3e06f022ff309e0f40f04d86c6fba226b34504d8
Deepanshu Dutta <deepanshudutta321@gmail.com>
2023-06-08 23:20:09 +0530

    Update 1)Set Matrix Zeros.cpp

--- 1)Set Matrix Zeros.cpp
+++ 1)Set Matrix Zeros.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-//Brute force
+//Brute force tc(o(n*m)(n+m)) sc(o(1))
 void mark_row(vector<vector<int>> &matrix,int row)
 {
     for(int j=0;j<matrix[0].size();j++)
@@ -51,3 +51,38 @@ int main()
 
     return 0;
 }
+
+// optimized soltion
+//tc(2*n*m) sc(o(n)+o(m))
+int main()
+{
+     vector<vector<in>> matrix;
+     // create two dummy array
+     int row[n]={0};
+     int col[m]={0};
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(matrix[i][j]==0)
+            {
+                row[i]=1;
+                col[j]=1;
+            }
+        }
+    }
+    // just traverse the matrix linerly  and check if the col or row is 1 or not then just put zero there 
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;i<m;j++)
+        {
+            if(row[i] or col[i])
+            matrix[i][j]=1;
+        }
+    }
+    
+    
+    return 0;
+}
+
