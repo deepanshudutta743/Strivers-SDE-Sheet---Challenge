84ea648ea69162f40d18ac02fd0ac5014a02a546
Deepanshu Dutta <deepanshudutta321@gmail.com>
2023-06-09 20:22:34 +0530

    all updated

--- 1)Set Matrix Zeros.cpp
+++ 1)Set Matrix Zeros.cpp
@@ -58,31 +58,93 @@ int main()
 {
      vector<vector<in>> matrix;
      // create two dummy array
-     int row[n]={0};
-     int col[m]={0};
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)
-        {
-            if(matrix[i][j]==0)
-            {
-                row[i]=1;
-                col[j]=1;
+
+
+	int n=matrix.size();
+	int m=matrix[0].size();
+	    int row[n] = {0}; // row array
+    int col[m] = {0}; // col array
+
+    // Traverse the matrix:
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                // mark ith index of row wih 1:
+                row[i] = 1;
+
+                // mark jth index of col wih 1:
+                col[j] = 1;
             }
         }
     }
-    // just traverse the matrix linerly  and check if the col or row is 1 or not then just put zero there 
-    
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;i<m;j++)
-        {
-            if(row[i] or col[i])
-            matrix[i][j]=1;
+
+    // Finally, mark all (i, j) as 0
+    // if row[i] or col[j] is marked with 1.
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0;
+            }
         }
     }
-    
     
     return 0;
 }
 
+// most optimized
+int main()
+{
+    	int n=matrix.size();
+	int m=matrix[0].size();
+
+           int col0=1;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+          if(matrix[i][j]==0)
+          {
+              //mark the ith row
+              matrix[i][0]=0;
+              //mark the ith col
+              if(j!=0)
+              matrix[0][j]=0;
+              else
+              col0=0;
+              
+          }
+        }
+
+    }
+            for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(matrix[i][j]!=0)
+                {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                {
+                    matrix[i][j]=0;
+                    
+                }
+                }
+            }
+        }
+    
+    // row part
+    if(matrix[0][0]==0)
+    {
+        for(int j=0;j<m;j++)
+        matrix[0][j]=0;
+    }
+    //column part
+    if(col0==0)
+    {
+        for(int i=0;i<n;i++)
+        {
+            matrix[i][0]=0;
+        }
+    }
+
+    return 0;
+}
