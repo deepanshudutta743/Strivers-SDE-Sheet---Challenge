e159560f5f378898cd40bd119864b49a4c8b8fb1
Deepanshu Dutta <deepanshudutta321@gmail.com>
2023-06-11 00:09:10 +0530

    basic code

---
+++ 3)sort012.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   //   Write your code here
+   //brute force
+   // sort(arr,arr+n);
+
+// int c0=0,c1=0,c2=0;
+
+//optimal o(2n)
+// for(int i=0;i<n;i++)
+// {
+  
+// if(c0==0) c0++;
+// else if(c1==1) c1++;
+// else c2++;
+// }
+
+// for(int i=0;i<c0;i++)
+// {
+//    arr[i]=0;
+// }
+// for(int i=c0;i<c1+c0;i++)
+// {
+//    arr[i]=1;
+// }
+// for(int i=c1+c0;i<n;i++)
+// {
+//    arr[i]=2;
+// }
+
+// most optimized
+//it uses dutch national flag algorithm
+
+
+}
