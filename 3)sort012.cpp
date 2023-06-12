6a7a91d5ae0687148270c4da97f584c1a3b70fab
Deepanshu Dutta <deepanshudutta321@gmail.com>
2023-06-12 20:00:58 +0530

    all aptimal

--- 3)sort012.cpp
+++ 3)sort012.cpp
@@ -33,4 +33,24 @@ void sort012(int *arr, int n)
 //it uses dutch national flag algorithm
 
 
+int low=0,mid=0,high=n-1;
+
+while(mid<=high)
+{
+   if(arr[mid]==0)
+   {
+     swap(arr[low],arr[mid]);
+     low++;
+     mid++;
+   }
+   else if(arr[mid]==1)
+   {
+     mid++;
+   } else {
+
+     swap(arr[mid], arr[high]);
+     high--;
+   }
+}
+
 }
